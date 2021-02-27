049502a5cc24ac1aba9b111b8a5f0b12eaabbed0
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2021-02-28 02:18:31 +0530

    Web API Basics

---
+++ MVC-Demo-2/Controllers/VideoApiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using MVC_Demo_2.Models;
+
+namespace MVC_Demo_2.Controllers
+{
+    public class VideoApiController : ApiController
+    {
+        VideoDb _db = new VideoDb();
+
+        // GET: api/VideoApi
+        public IEnumerable<Video> Get()
+        {
+            return _db.Videos;
+        }
+
+        // GET: api/VideoApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/VideoApi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/VideoApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/VideoApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- MVC-Demo-2/Global.asax.cs
+++ MVC-Demo-2/Global.asax.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
+using System.Web.Http;
 using System.Web.Routing;
+
 
 namespace MVC_Demo_2
 {
@@ -12,6 +14,7 @@ namespace MVC_Demo_2
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
